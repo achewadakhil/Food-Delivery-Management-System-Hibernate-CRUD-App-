67cf9a25fc6561789f32dce96bfca5d540fa37a9
achewadakhil <achewadakhil11@gmail.com>
2026-02-12 15:18:39 +0530

    Inserting data into table(Create)

--- food-delivery-manager/src/main/java/com/fooddelivery/App.java
+++ food-delivery-manager/src/main/java/com/fooddelivery/App.java
@@ -1,5 +1,11 @@
 package com.fooddelivery;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
 /**
  * Hello world!
  *
@@ -8,6 +14,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Customer c1 = new Customer();
+
+        c1.setId(1);
+        c1.setName("Akhil");
+
+        SessionFactory sf = new Configuration()
+                            .addAnnotatedClass(Customer.class)
+                            .configure()
+                            .buildSessionFactory();
+        Session session = sf.openSession();
+
+        
+        
+        session.persist(c1);
+        
+        Transaction tx = session.beginTransaction();
+        tx.commit();
+        sf.close();
+        session.close();
+        System.out.println(c1);
     }
 }
---
+++ food-delivery-manager/src/main/java/com/fooddelivery/Customer.java
@@ -0,0 +1,34 @@
+package com.fooddelivery;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+    
+
+    @Id
+    private int id;
+    private String name;
+
+    
+    @Override
+    public String toString() {
+        return "Customer [id=" + id + ", name=" + name + "]";
+    }
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+}
